1d7dd14a3358a3ab871eca91dd753805bf0de838
Mario Alberto Villagrana Hurtado <mvillagrana@focaltec.com>
2019-08-21 18:32:53 -0500

    Add sequelize for db connection

--- db/config.js
+++
@@ -1,8 +0,0 @@
-const sequelize = new Sequelize('','','', {
-  dialect: 'sqlite',
-  storage: './db/prueba.db'
-})
-
-// const sequelize = new Sequelize('sqlite:./prueba.db')
-
-module.exports = sequelize;
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,15 @@
+var models = require('./../models')
 var express = require('express');
 var router = express.Router();
-var db = require('../db/config')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.serialize(function() {
-    db.run('CREATE TABLE lorem (info TEXT)');
-    var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
-
-    for (var i = 0; i < 10; i++) {
-      stmt.run('Ipsum ' + 1);
-    }
-
-    stmt.finalize();
-
-    db.each('SELECT rowid as id, info FROM lorem', function(err, row) {
-      console.log(row.id + ': ' + row.info);
+  models.User.findAll().then(function(users) {
+    res.render('index', {
+      title: 'Sequelize: Express Example',
+      users: users
     });
   });
-
-  db.close();
-
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
